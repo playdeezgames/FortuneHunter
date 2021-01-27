c32522872ba70a7c97d2058fac0784dfacde1b42
ernestpazera <ernestpazera@gmail.com>
2021-01-27 05:30:28 -0600

    refactored magic strings for main menu

--- FortuneHunter/FortuneHunter/Constants/UI/MainMenu.h
+++ FortuneHunter/FortuneHunter/Constants/UI/MainMenu.h
@@ -5,5 +5,10 @@ namespace Constants::UI::MainMenu
 {
 	const int OFFSET_Y = Constants::Cell::HEIGHT * 2;
 	const int LINE_HEIGHT = Constants::UI::LINE_HEIGHT;
+	const std::string TITLE = "Welcome to Fortune Hunter!";
+	const std::string OPTION_START = "Start";
+	const std::string OPTION_INSTRUCTIONS = "Instructions";
+	const std::string OPTION_ABOUT = "About";
+	const std::string OPTION_OPTIONS = "Options";
+	const std::string OPTION_QUIT = "Quit";
 }
-
