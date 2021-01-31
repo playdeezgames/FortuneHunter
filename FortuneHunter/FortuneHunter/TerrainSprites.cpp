1d3b2295fd0bcfc23ee27c3243157140e3a09e3b
ernestpazera <ernestpazera@gmail.com>
2021-01-31 12:27:37 -0600

    moved terrainsprites into a sprite table

--- FortuneHunter/FortuneHunter/TerrainSprites.cpp
+++ FortuneHunter/FortuneHunter/TerrainSprites.cpp
@@ -1,21 +1,6 @@
 #include "TerrainSprites.h"
 #include "Utility.h"
-TerrainSpriteManager::TerrainSpriteManager()
-	: table()
+TerrainType TerrainSpriteManager::StringToIdentifier(const std::string& value)
 {
-}
-
-void TerrainSpriteManager::Start(const tggd::common::SpriteManager& spriteManager, const std::string& fileName)
-{
-	nlohmann::json j = tggd::common::Utility::LoadJSON(fileName);
-	for (auto& entry : j.items())
-	{
-		TerrainType terrainType = (TerrainType)tggd::common::Utility::StringToInt(entry.key());
-		table[terrainType] = spriteManager.GetSprite(entry.value());
-	}
-}
-
-const tggd::common::Sprite* TerrainSpriteManager::Get(TerrainType terrainType) const
-{
-	return table.find(terrainType)->second;
+	return (TerrainType)tggd::common::Utility::StringToInt(value);
 }
--- FortuneHunter/FortuneHunter/TerrainSprites.h
+++ FortuneHunter/FortuneHunter/TerrainSprites.h
@@ -1,14 +1,8 @@
 #pragma once
-#include <map>
-#include <string>
 #include "TerrainType.h"
-#include "SpriteManager.h"
-class TerrainSpriteManager
+#include "SpriteTable.h"
+class TerrainSpriteManager: public tggd::common::SpriteTable<TerrainType>
 {
-private:
-	std::map<TerrainType, const tggd::common::Sprite*> table;
-public:
-	TerrainSpriteManager();
-	void Start(const tggd::common::SpriteManager&,const std::string&);
-	const tggd::common::Sprite* Get(TerrainType) const;
+protected:
+	TerrainType StringToIdentifier(const std::string&);
 };
