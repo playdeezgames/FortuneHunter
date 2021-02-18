43eee3bbede7c8f548c277e5435c906b07486691
ernestpazera <ernestpazera@gmail.com>
2021-02-18 04:49:17 -0600

    final score screen is done

--- FortuneHunter/FortuneHunter/FinalScoreRenderer.cpp
+++ FortuneHunter/FortuneHunter/FinalScoreRenderer.cpp
@@ -19,18 +19,21 @@ void FinalScoreRenderer::Draw() const
 {
 	BaseMenuRenderer::Draw();
 	//TODO: magic numbers vvv
-	GetFont().WriteText(GetRenderer(), tggd::common::XY(0, 0), "Final Score:", Constants::Color::GREEN);
+	GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(324, 100), "==Final Score==", Constants::Color::BLACK);
+	GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(320, 96), "==Final Score==", Constants::Color::GREEN);
 
-	int y = 32;//TODO: magic number!
+	int y = 192;//TODO: magic number!
 	for (auto award : HunterAwardHelper::GetAll())
 	{
 		std::stringstream ss;
 		ss << HunterAwardHelper::GetName(award) << " x" << gameData.GetHunter()->GetTally(award) << " -> " << gameData.GetHunter()->GetScore(award);//TODO: magic strings
-		GetFont().WriteText(GetRenderer(), tggd::common::XY(0, y), ss.str(), Constants::Color::GRAY);
-		y += 16;//TODO: magic number
+		GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(324, y+4), ss.str(), Constants::Color::BLACK);
+		GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(320, y), ss.str(), Constants::Color::GRAY);
+		y += 24;//TODO: magic number
 	}
-	y += 16;//TODO: MAGIC
+	y += 32;//TODO: MAGIC
 	std::stringstream ss;
-	ss << "Final Score: " << gameData.GetHunter()->GetScore();
-	GetFont().WriteText(GetRenderer(), tggd::common::XY(0, y), ss.str(), Constants::Color::CYAN);
+	ss << "Total Score: " << gameData.GetHunter()->GetScore();
+	GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(324, y+4), ss.str(), Constants::Color::BLACK);
+	GetFont().WriteTextCentered(GetRenderer(), tggd::common::XY(320, y), ss.str(), Constants::Color::CYAN);
 }
